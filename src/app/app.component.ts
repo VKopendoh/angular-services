c7cee5cd8f5188ec53e848331a160c1110177eaf
vkopendoh <vkopendoh@bk.ru>
2020-02-21 12:04:29 +0300

    Services!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AppCounterService} from './services/app-counter.service';
+import {LocalCounterService} from './services/local-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LocalCounterService] // that's how to provide local service
 })
 export class AppComponent {
-  title = 'ang-services';
+  // DI service via constructor
+  constructor(
+    private appCounterService: AppCounterService,
+    private localCounterService: LocalCounterService) {
+  }
 }
